6ddccce9d5f386afbfec07d745ac289b46f98387
Christopher Kaster <atomicptr@gmail.com>
2017-04-22 13:38:30 +0200

    change movement to be acceleration based, add stars into background

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,17 +1,42 @@
 extends KinematicBody2D
 
+onready var particles = get_node("particles")
+
+const ACCELERATION = 7.0
+
+var velocity = Vector2(0, 0)
+
+var is_in_earth_collider = false
+
 func _ready():
     set_process(true)
 
 func _process(delta):
+
     if Input.is_action_pressed("left"):
-        move(Vector2(-1, 0))
+        velocity.x -= (ACCELERATION * 0.8)
 
     if Input.is_action_pressed("right"):
-        move(Vector2(1, 0))
+        velocity.x += (ACCELERATION * 0.8)
 
     if Input.is_action_pressed("up"):
-        move(Vector2(0, -1))
+        velocity.y -= ACCELERATION
 
     if Input.is_action_pressed("down"):
-        move(Vector2(0, 1))
+        velocity.y += ACCELERATION
+
+    if is_in_earth_collider:
+        velocity.y -= (ACCELERATION * 1.5)
+
+    print(velocity)
+
+    move(velocity * delta)
+    velocity *= 0.96
+
+func _on_earth_body_enter(body):
+    if body.is_in_group("player"):
+        is_in_earth_collider = true
+
+func _on_earth_body_exit(body):
+    if body.is_in_group("player"):
+        is_in_earth_collider = false
